52a33e5a43e09f855d6bd7b1bc9ed8c5b224583f
Felipe <felipe.santos@dogandponystudios.com>
2021-06-03 19:58:09 -0300

    Update hero effects and images

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-var pageContainer = document.querySelector('.scroll-wrap');
+var pageContainer = document.querySelector('[data-scroll-container]');
 
 var scroller = new LocomotiveScroll({
     el: pageContainer,
@@ -27,120 +27,39 @@ ScrollTrigger.scrollerProxy(pageContainer, {
 });
 
 window.addEventListener('load', function () {
-    var pinnedImages = gsap.utils.toArray('.hero__image');
-    var pinWrap = document.querySelector('.hero__pin');
-    var pinWrapWidth = pinWrap.offsetWidth;
-    var horizontalScrollLength = pinWrapWidth - window.innerWidth;
+    var docStyle = getComputedStyle(document.documentElement);
     var headerLogo = document.querySelector('.header__logo');
+    var largeLogo = document.querySelector('.large-logo');
 
     var tl1 = gsap.timeline({
         scrollTrigger: {
             scroller: pageContainer,
             trigger: '.hero',
-            start: 'top top',
-            end: pinWrapWidth,
+            start: 'bottom 65%',
+            end: 'bottom top',
             scrub: true,
-            pin: true,
+            // markers: true,
         },
     });
 
     var tl2 = gsap.timeline({
         scrollTrigger: {
             scroller: pageContainer,
-            trigger: '.about',
-            start: 'top bottom',
-            end: 'top bottom-=385px',
-            scrub: true,
+            trigger: '.hero',
+            start: 'bottom top',
+            toggleActions: 'play pause resume reset',
         },
     });
 
-    var tl3 = gsap.timeline({
-        scrollTrigger: {
-            scroller: pageContainer,
-            trigger: '.about',
-            start: 'top bottom-=385px',
-            toggleActions: "play pause resume reset",
-        },
-    });
+    tl1.to(largeLogo, { width: '176px', transformOrigin: '50% 0%' }, 'logo')
+        .to('html', { '--background': docStyle.getPropertyValue('--pink') }, 'logo');
 
-    tl1.to('.hero__pin', {  x: -horizontalScrollLength }, "start")
-        .to('.large-logo-container', { x: horizontalScrollLength }, "start");
-
-    pinnedImages.forEach(function (image, index) {
-        tl1.to(image, { xPercent: - index * 59 }, "start");
-    });
-
-    tl2.to('.large-logo-container', { width: '176px' });
-
-    tl3.to('.large-logo-container', { opacity: 0 })
+    tl2.to(largeLogo, { opacity: 0 })
         .to(headerLogo, { opacity: 1 });
 
     gsap.timeline()
         .add(tl1)
-        .add(tl2)
-        .add(tl3);
-
-    // var tl2 = gsap.timeline({
-    //     scrollTrigger: {
-    //         trigger: '.about',
-    //         scrub: true,
-    //         pin: true,
-    //         start: 'top top',
-    //         end: 'top center',
-    //     },
-    // });
-
-    // tl2.to('.logo-container', { scale: .15 })
-
-    // gsap.to('.hero__pin', {
-    //     scrollTrigger: {
-    //         // scroller: pageContainer,
-    //         scrub: true,
-    //         trigger: '.hero',
-    //         pin: true,
-    //         anticipatePin: 1,
-    //         start: 'top top',
-    //         end: pinWrapWidth
-    //     },
-    //     x: -horizontalScrollLength,
-    //     ease: "none"
-    // });
-
-    // gsap.to('.logo-container', {
-    //     scrollTrigger: {
-    //         // scroller: pageContainer,
-    //         scrub: true,
-    //         start: 'top top',
-    //         end: pinWrapWidth
-    //     },
-    //     x: horizontalScrollLength,
-    //     ease: "none"
-    // });
-
-    // gsap.to('.logo-container', {
-    //     scrollTrigger: {
-    //         // scroller: pageContainer,
-    //         scrub: true,
-    //         trigger: '.about',
-    //         pin: true,
-    //         start: 'top bottom',
-    //         end: 'top 70%',
-    //     },
-    //     scale: .15,
-    //     y: 0,
-    // });
-
-    // gsap.to('#logo', {
-    //     scrollTrigger: {
-    //         scroller: pageContainer,
-    //         scrub: true,
-    //         trigger: '.about',
-    //         endTrigger: '#hero',
-    //         start: 'top bottom',
-    //         end: 'top center',
-    //     },
-    //     scale: .2,
-    // });
+        .add(tl2);
 
     ScrollTrigger.addEventListener('refresh', () => scroller.update());
     ScrollTrigger.refresh();
